9adc71f023dd6a6a45211fde67015e20033bd12c
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-09-11 16:07:28 -0400

    [APP LAYOUT] : Set up & designed Results component.

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -6,9 +6,45 @@ import './results.css';
 
 const Results = () => {
     return (
-        <div>
-            <h1>Results</h1>
-        </div>
+        <>
+            <div className="results-title">
+                <h2><u>Results</u></h2>
+            </div>
+
+            <div className="results">
+
+                <div>
+                    <h4><strong>1234</strong></h4>
+                    <em>Amount to borrow</em>
+                </div>
+
+                <div>
+                    <h4><strong>2345</strong></h4>
+                    <em>Monthly payment</em>
+                </div>
+
+                <div>
+                    <h4><strong>3456</strong></h4>
+                    <em>Total Repaid</em>
+                </div>
+
+                <div>
+                    <h4><strong>4567</strong></h4>
+                    <em>Total Interest Paid</em>
+                </div>
+
+                <div>
+                    <h4><strong>5678</strong></h4>
+                    <em>Deposit</em>
+                </div>
+
+                <div>
+                    <h4><strong>6789</strong></h4>
+                    <em>Loan to value</em>
+                </div>
+
+            </div>
+        </>
     )
 }
 
