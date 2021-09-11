ee2078c413b7cc1c31abbf1cd7865783b74cc1eb
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-09-11 17:02:13 -0400

    [STATE MANAGEMENT] : Used states from Context Api in Results component to display final mortgage values.

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,10 +1,28 @@
 // Packages
-import React from 'react';
+import React, { useContext } from 'react';
 
-// Files
+// Contexts
+import { calculatorValues } from 'contexts/calculator-context';
+
+// Others
 import './results.css';
 
 const Results = () => {
+
+    const {
+        purchasingPrice,
+        deposit,
+        mortgageTerm,
+        interestRate,
+    } = useContext(calculatorValues);
+
+    const borrowingAmount = purchasingPrice - deposit;
+    const monthlyPayment = (borrowingAmount * (interestRate / 100 / 12)) / (1 - (((interestRate / 100 / 12) + 1) ** (12 * (0 - mortgageTerm))));
+    const totalRepaid = monthlyPayment * 12 * mortgageTerm;
+    const totalInterestPaid = totalRepaid - borrowingAmount;
+    const depositPercentage = deposit / purchasingPrice * 100;
+    const loanToValue = borrowingAmount / purchasingPrice * 100;
+
     return (
         <>
             <div className="results-title">
@@ -14,32 +32,32 @@ const Results = () => {
             <div className="results">
 
                 <div>
-                    <h4><strong>1234</strong></h4>
+                    <h4><strong>${borrowingAmount.toFixed(2)}</strong></h4>
                     <em>Amount to borrow</em>
                 </div>
 
                 <div>
-                    <h4><strong>2345</strong></h4>
+                    <h4><strong>${monthlyPayment.toFixed(2)}</strong></h4>
                     <em>Monthly payment</em>
                 </div>
 
                 <div>
-                    <h4><strong>3456</strong></h4>
+                    <h4><strong>${totalRepaid.toFixed(2)}</strong></h4>
                     <em>Total Repaid</em>
                 </div>
 
                 <div>
-                    <h4><strong>4567</strong></h4>
+                    <h4><strong>${totalInterestPaid.toFixed(2)}</strong></h4>
                     <em>Total Interest Paid</em>
                 </div>
 
                 <div>
-                    <h4><strong>5678</strong></h4>
+                    <h4><strong>{depositPercentage.toFixed(2)}%</strong></h4>
                     <em>Deposit</em>
                 </div>
 
                 <div>
-                    <h4><strong>6789</strong></h4>
+                    <h4><strong>{loanToValue.toFixed(2)}%</strong></h4>
                     <em>Loan to value</em>
                 </div>
 
