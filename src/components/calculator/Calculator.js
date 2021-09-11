2b0970f7e7708b57741d7b64116aa292c75edfd3
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-09-11 16:04:31 -0400

    [APP LAYOUT] : Set up Calculator component using Ant Design.

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,14 +1,77 @@
 // Packages
 import React from 'react';
+import { Slider } from 'antd';
 
 // Files
 import './calculator.css'
 
 const Calculator = () => {
+
+    const siteColor = '#073b07';
+
     return (
-        <div>
-            <h1>Calculator</h1>
-        </div>
+        <>
+
+            <div className="flex calculator-title">
+                <h2><u>Mortgage Variables</u></h2>
+            </div>
+
+            <div className="grid-2 sliders">
+
+                <div className="center">
+                    <h4>10000</h4>
+                    <Slider
+                        handleStyle={{ borderColor: siteColor }}
+                        trackStyle={{ backgroundColor: siteColor }}
+                        defaultValue={10000}
+                        min={0}
+                        max={1000000}
+                        step={500}
+                    />
+                    <em>Purchasing House Price</em>
+                </div>
+
+                <div className="center">
+                    <h4>20000</h4>
+                    <Slider
+                        handleStyle={{ borderColor: siteColor }}
+                        trackStyle={{ backgroundColor: siteColor }}
+                        defaultValue={20000}
+                        min={0}
+                        max={300000}
+                        step={500}
+                    />
+                    <em>Deposit</em>
+                </div>
+
+                <div className="center">
+                    <h4>2</h4>
+                    <Slider
+                        handleStyle={{ borderColor: siteColor }}
+                        trackStyle={{ backgroundColor: siteColor }}
+                        defaultValue={2}
+                        min={5}
+                        max={40}
+                        step={1}
+                    />
+                    <em>Mortgage Term</em>
+                </div>
+
+                <div className="center">
+                    <h4>2</h4>
+                    <Slider
+                        handleStyle={{ borderColor: siteColor }}
+                        trackStyle={{ backgroundColor: siteColor }}
+                        defaultValue={2}
+                        min={0.1}
+                        max={10}
+                        step={0.1}
+                    />
+                    <em>Interest Rate</em>
+                </div>
+
+            </div>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// Packages
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Files
 import 'css/index.css';
+import 'antd/dist/antd.css';
 import App from 'components/App';
 
 ReactDOM.render(
