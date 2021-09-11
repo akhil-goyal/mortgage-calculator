e4dd41434c99adc6506b4c502184da53b5c5546c
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-09-11 16:15:48 -0400

    [APP TESTING] : Set up tests for App to check if Header, Calculator & Results components are being mounted.

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,27 @@
+// Packages
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// Components
+import App from 'components/App';
+import Header from 'components/header/Header';
+import Calculator from 'components/calculator/Calculator';
+import Results from 'components/results/Results';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
+
+it('displays HEADER component', () => {
+    expect(wrapped.find(Header).length).toEqual(1);
+});
+
+it('displays CALCULATOR component', () => {
+    expect(wrapped.find(Calculator).length).toEqual(1);
+});
+
+it('displays RESULTS component', () => {
+    expect(wrapped.find(Results).length).toEqual(1);
+});
